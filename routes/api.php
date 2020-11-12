a82521b66d385001f23a393f999f6e44873146bd
haterphp <54400838+haterphp@users.noreply.github.com>
2020-11-12 12:39:00 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,20 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('logout', [UserController::class, 'logout'])->middleware(['auth:api']);
+Route::post('register', [UserController::class, 'store']);
+Route::post('login', [UserController::class, 'login']);
+Route::get('branch', [BranchController::class, 'index']);
+Route::post('booking', [BookingController::class, 'store']);
+Route::get('cars', [CarController::class, 'search']);
+
 Route::get('booking/history', [BookingController::class, 'history'])->middleware('auth:api');
 Route::patch('booking/{booking}/close', [BookingController::class, 'close'])->middleware('auth:api');
 
-Route::prefix('api')->group(function (){
-    Route::get('logout', [UserController::class, 'logout'])->middleware(['auth:api']);
-    Route::post('register', [UserController::class, 'store']);
-    Route::post('login', [UserController::class, 'login']);
-    Route::get('branch', [BranchController::class, 'index']);
-    Route::post('booking', [BookingController::class, 'store']);
-    Route::get('cars', [CarController::class, 'search']);
+Route::get('profile', [UserController::class, 'view'])->middleware('auth:api');
+Route::get('booking/active', [BookingController::class, 'active'])->middleware('auth:api');
+Route::get('booking/{booking}', [BookingController::class, 'view'])->middleware('auth:api');
 
-    Route::get('profile', [UserController::class, 'view'])->middleware('auth:api');
-    Route::get('booking/active', [BookingController::class, 'active'])->middleware('auth:api');
-    Route::get('booking/{booking}', [BookingController::class, 'view'])->middleware('auth:api');
-});
 
 
